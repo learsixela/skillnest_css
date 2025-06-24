4a8bd4e3750ab2a8092b057eaf23d38b6a768a1c
Israel <ipalma@gmail.com>
2025-06-23 21:38:57 -0400

    Manejo de eventos

--- pagina2/assets/js/dom_script.js
+++ pagina2/assets/js/dom_script.js
@@ -8,3 +8,27 @@ if(boton3){
     alert("El elemento NO existe en el DOM");
 }
 
+/* EVENTOS */
+//1.- capturar el elemento
+const boton_clickear = document.querySelector('#clickear');
+//2.- validar si existe, 
+
+if(boton_clickear){
+    //3.- capturar el evento
+    boton_clickear.addEventListener('click',function(){
+        alert("¡Botón clickeado!");
+
+    });
+
+    boton_clickear.addEventListener('mouseover', function() {
+        console.log('El mouse está sobre el botón');
+    });
+}
+
+let input_nombre = document.querySelector('#nombre');//1
+
+if(input_nombre){//2
+    input_nombre.addEventListener('change',function(){//3
+        alert("input cambio...");
+    });
+}
