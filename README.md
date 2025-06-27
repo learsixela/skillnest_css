65bb0dadbbc426176ea3648f798425fafe15f034
Israel <ipalma@gmail.com>
2025-06-26 21:55:36 -0400

    trabajao en git, github y  rama

--- README.md
+++ README.md
@@ -12,3 +12,34 @@ Directamente sobre la etiqueta HTML, ejemplo
 ```
 <p class="pcolor" id="p5" style="color: cyan;">Parrafo 5</p>
 ```
+
+
+- rw- r-- r--
+- 6 4 4
+r(4) read
+w(2) write
+x(1) execute
+
+1er trio usuario
+2do trio grupo del usuario
+3er trio usuario invitado/o perteneciente a otro grupo
+
+
+chmod 400
+chmod 777
+
+ls -la           #muestra todos los archivos
+git clone https://github.com/learsixela/repo_prueba0078.git repo_isra
+cd ..           #retroceder  un directorio
+cd repo_isra    #ingresar al directorio clonado     
+code .          #abrir vscode     
+
+
+## Ramas 
+git branch israel_palma //creo la rama
+
+git branch -a //veo las ramas
+
+git checkout israel_palma //cambiar de rama
+
+git checkout -b palma_israel// crear y cambiarme de rama
--- pagina2/assets/js/dom_script.js
+++ pagina2/assets/js/dom_script.js
@@ -1,3 +1,9 @@
+        /*agregar elementos al DOM appendChild()*/
+        let nuevoParrafo = document.createElement('p');//crear el elemento p
+        nuevoParrafo.textContent = 'Este es un párrafo agregado dinámicamente.';
+        document.body.appendChild(nuevoParrafo);
+        let estilo = nuevoParrafo.style.color;
+
 const boton3 = document.querySelector('#trabajar');//no existe
 
 console.log(boton3);//null
@@ -17,12 +23,20 @@ if(boton_clickear){
     //3.- capturar el evento
     boton_clickear.addEventListener('click',function(){
         alert("¡Botón clickeado!");
-
+        if(nuevoParrafo.style.color ==""){
+            nuevoParrafo.setAttribute('style','color: red;');
+        }else{
+            nuevoParrafo.style.color ="";
+        }
+        document.body.style.backgroundColor='black';
+        
     });
 
     boton_clickear.addEventListener('mouseover', function() {
         console.log('El mouse está sobre el botón');
     });
+
+    
 }
 
 let input_nombre = document.querySelector('#nombre');//1
